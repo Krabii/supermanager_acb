f10b905de553df1915ae390d708e48632ccd1f00
Krabii <leonidas.yovan@gmail.com>
2024-10-13 17:41:59 +0200

    init

---
+++ main.py
@@ -0,0 +1,37 @@
+from typing import List
+from dataclasses import dataclass
+from ortools.sat.python import cp_model
+
+
+@dataclass
+class Journey:
+    "This class is the points a player had in a concrete game (Journey)"
+    numberJourney: int
+    pointsJourney: int
+
+
+@dataclass
+class BasketPlayer:
+    """Class for ABC Supermanager Player."""
+    age: int
+    competitionAverage: float
+    down15: float
+    fisicStatus: str
+    fullName: str
+    height: str
+    initialPrice: float
+    injuredDays: int
+    isActive: bool
+    isBlock: bool
+    isExitLeague: bool
+    isExtraCommunity: bool
+    isNational: bool
+    keep: float
+    license: str
+    nameTeam: str
+    nick: str
+    playerStats: List
+    position: int
+    price: float
+    shortName: str
+    up15: float
